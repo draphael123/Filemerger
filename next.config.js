c54031d2d196f45381cafe84314805e5d04bc773
Daniel Raphael <danielraphaelh@gmail.com>
2026-01-03 17:33:45 -0500

    Fix Vercel build: Add missing type definitions and ESLint dependencies

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  api: {
-    bodyParser: {
-      sizeLimit: '100mb',
-    },
-  },
   experimental: {
     serverActions: {
       bodySizeLimit: '100mb',
